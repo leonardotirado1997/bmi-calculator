1441cd43162f7eab27ef532c2c7da9c85a57036b
Leonardo Pires Tirado <leonardo.tirado@docente.pr.senac.br>
2025-03-20 16:50:38 -0300

    feat: add field validation in the form before inserting into the table

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,42 +1,60 @@
 // Cálculo do IMC do paciente
 var pacientes = document.querySelectorAll(".paciente");
- 
+
 for (var i = 0; i < pacientes.length; i++) {
     const paciente = pacientes[i];
-    var tdPeso = paciente.querySelector(".info-peso");
-    var tdAltura = paciente.querySelector(".info-altura");
- 
-    var tdImc = paciente.querySelector(".info-imc");
- 
+
+    var peso = paciente.querySelector(".info-peso").textContent;
+    var altura = paciente.querySelector(".info-altura").textContent;
+
     var alturaEhValida = true;
     var pesoEhValido = true;
- 
-    var peso = tdPeso.textContent;
-    if (peso <= 0 || peso > 600) {
-        console.log("Peso inválido!");
-        pesoEhValido = false;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var alturaEhValida = validaAltura(altura);
+    var pesoEhValido = validaPeso(peso);
+
+    // var peso = tdPeso.textContent;
+
+    if (!pesoEhValido) {
         tdImc.textContent = "Peso inválido!"
     }
- 
-    var altura = tdAltura.textContent;
-    if (altura <= 0 || altura > 3) {
-        console.log("Altura inválida!");
-        alturaEhValida = false;
+    
+    // var altura = tdAltura.textContent;
+
+    if (!alturaEhValida) {
         tdImc.textContent = "Altura inválida!";
     }
- 
+
     if (!pesoEhValido && !alturaEhValida) {
         tdImc.textContent = "Altura e/ou peso inválidos!"
     }
- 
+
     if (alturaEhValida && pesoEhValido) {
         var imc = calculaImc(peso, altura);
         tdImc.textContent = imc;
     }
 }
- 
+
 function calculaImc(peso, altura) {
     var imc = 0;
     imc = peso / Math.pow(altura, 2);
     return imc.toFixed(2);
+}
+
+function validaPeso(peso) {
+    if (peso >= 0 && peso < 600) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validaAltura(altura) {
+    if (altura > 0 && altura < 3) {
+        return true;
+    } else {
+        false;
+    }
 }
--- js/form.js
+++ js/form.js
@@ -6,6 +6,13 @@ botaoAdicionar.addEventListener("click", function (event) {
     var form = document.querySelector("#form-adiciona");
 
     var paciente = obtemPacienteDoFormulario(form);
+    
+    var erros = validaPaciente(paciente);
+
+    if (erros.length > 0){
+        exibeMensagemErro(erros);
+        return;
+    }
 
     if (!paciente.altura || !paciente.gordura || !paciente.nome || !paciente.peso) {
     var validacao = document.querySelector("#MsgErro")
@@ -54,4 +61,40 @@ function montaTr(paciente) {
 
     return pacienteTr;
 }
- 
+
+function validaPaciente(paciente) {
+    var erros = [];
+
+    if (paciente.nome.length == 0) {
+        erros.push("Nome é obrigatório!");
+    }
+    if (paciente.altura.length == 0){
+        erros.push("Altura é obrigatório!");
+    }
+    if (paciente.peso.length == 0){
+        erros.push("Peso é obrigatório!");
+    }
+    if (paciente.gordura.length == 0){
+        erros.push("Gordura é obrigatório!");
+    }
+    if (!validaPeso(paciente.peso)){
+        erros.push("Peso é inválido.");
+    }
+    if (!validaAltura(paciente.altura)){
+        erros.push("Altura é inválida!");
+    }
+
+    return erros;
+    
+}
+
+function exibeMensagemErro(erros){
+    var ul = document.querySelector("#MsgErro");
+    ul.innerHTML = "";
+    
+    erros.forEach(erro => {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    })
+}
