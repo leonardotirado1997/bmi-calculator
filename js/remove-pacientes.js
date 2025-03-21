927e1537d92432af9c3a883e00f61848051fb014
Leonardo Pires Tirado <leonardo.tirado@docente.pr.senac.br>
2025-03-21 17:03:12 -0300

    feat: implement delete functionality, changing double click to single click

--- js/remove-pacientes.js
+++ js/remove-pacientes.js
@@ -1,6 +1,6 @@
 var tabela = document.querySelector("#tabela-pacientes");
 
-tabela.addEventListener("dblclick", function (event) {
+tabela.addEventListener("click", function (event) {
     if (event.target.classList.contains("fa-trash-alt")) {
     event.target.parentNode.parentNode.parentNode.classList.add("fadeOut");
     
